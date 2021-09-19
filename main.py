9921996c032105cbb06e197e54d430768fc17b68
nano-ssim <90839753+nano-ssim@users.noreply.github.com>
2021-09-19 18:20:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+def get_phones():
+   Number_phone = []
+   try:
+      Tels = re.findall("[+][\d]{3}[ ][(][\d][)][ ][\d]{2}[ ][\d]{2}[ ][\d]{2}[ ][\d]{2}", response.text)
+      Number_phone = Number_phone + Tels 
+   except:
+      pass
+   try:
+      Tels = re.findall("[+][\d]{3}[ ][\d]{2}[ ][\d]{2}[ ][\d]{2}[ ][\d]{2}", response.text)
+      Number_phone = Number_phone + Tels 
+   except:
+      pass
+   try:
+      Tels = re.findall("[+][\d]{3}[ ][(][\d][)][ ][\d]{3}[ ][\d]{2}[ ][\d]{2}[ ][\d]{2}", response.text)
+      Number_phone = Number_phone + Tels 
+   except:
+      pass
+   return Number_phone
+  
+        
+def get_mails():
+   Liste_Mails = []
+   try:
+      Mails = re.findall("([A-z0-9\.]+@[A-z0-9]+\.[A-z.]+)", response.text)
+      Liste_Mails = Liste_Mails + Mails
+   except:
+      pass
+   return Liste_Mails
+
+
+# Here you should put your urls in (str)
+urls = [url1,url2]
+
+for url in urls:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    for Tel in get_phones():
+         print (Tel)
+         
+    for Mail in get_mails():
+         print (Mail)
